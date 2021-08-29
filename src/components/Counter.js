2cb6f7743bd1565ca12e9a148abd62625bfae478
Abhinav Saurav <abhinavsaurav1@gmail.com>
2021-08-29 15:30:18 +0530

    redux toolkit and class redux demo

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -11,95 +11,126 @@
  * 
  * to dispatch the action we can make use of useDispatch
  */
-import { Component } from 'react';
-import { useSelector, useDispatch, connect } from 'react-redux';
-import classes from './Counter.module.css';
+// import { Component } from 'react';
+import { 
+   useSelector,
+   useDispatch,
+  //  connect 
+  } from 'react-redux';
+import { counterActions  } from '../store/index';
 
-// const Counter = () => {
-//   // it takes a function which will then be executed by react-redux a function which will determine which piece of data
-//   // we want to extract from our store
-//   // we will receive the state managed by redux and then we will basically ask what we want which then gets returned 
-//   // Also using this react-redux will auto enable the subscription for it . Also on unmounting react-redux will automatically remove the subsription for us
-//   const counter = useSelector(state => state.counter);
-//   const dispatch = useDispatch(); // don't need to pass a arg but we can now make use of the intialization to dispatch our actions
+  import classes from './Counter.module.css';
 
-//   const incrementHandler = () => {
-//     dispatch({type: 'increment'});
-//   };
+const Counter = () => {
+  // it takes a function which will then be executed by react-redux a function which will determine which piece of data
+  // we want to extract from our store
+  // we will receive the state managed by redux and then we will basically ask what we want which then gets returned 
+  // Also using this react-redux will auto enable the subscription for it . Also on unmounting react-redux will automatically remove the subsription for us
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch(); // don't need to pass a arg but we can now make use of the intialization to dispatch our actions
 
-//   const decrementHandler = () => {
-//     dispatch({type : 'decrement'});
-//   };
+  const show = useSelector(state => state.showCounter);
 
+  const incrementHandler = () => {
+    
+    dispatch(counterActions.increment()); 
 
-//   const toggleCounterHandler = () => {};
-
-//   return (
-//     <main className={classes.counter}>
-//       <h1>Redux Counter</h1>
-//       <div className={classes.value}>{counter}</div>
-//       <div>
-//       <button onClick={incrementHandler}>Increment</button>
-//       <button onClick={decrementHandler}>Decrement</button>
-//       </div>
-//       <button onClick={toggleCounterHandler}>Toggle Counter</button>
-//     </main>
-//   );
-// };
-
-
-class Counter extends Component{
-
-  incrementHandler(){
-    this.props.increment();
-  }
-
-  decrementHandler(){
-    this.props.decrement();
-  }
-
-  toggleCounterHandler(){
-
-  }
-
-  render(){
-    return (
-      <main className={classes.counter}>
-        <h1>Redux Counter</h1>
-        <div className={classes.value}>{this.props.counter}</div>
-        <div>
-          {/* we are binding to make sure the this keyword inside of the method refers to the class */}
-        <button onClick={this.incrementHandler.bind(this)}>Increment</button>
-        <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
-        </div>
-        <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
-      </main>
-    );
-  }
-}
-
-// this is the function which receives the redux state and will 
-const mapStateToProps = state => {
-  /**
-   * we pick the counter value from state and we are binding it here to the other
-   */
-  return {
-    counter: state.counter
+    // dispatch({type: 'increment'});
   };
-}
 
-// equivalent to useDispatch and will be executed for us by redux
-const mapDispatchToProps = dispatch => {
-  /**
-   * the keys are prop names which we can then use in the component and the value is another function 
-   * in which we call dispatch and we set-up dispatch
-   */
-  return {
-    increment: () => dispatch({type: 'increment'}),
-    decrement: () => dispatch({type: 'decrement'})
-  }
+  const increaseHandler = () => {
+    // we can pass the payload directly in the method like a number or an object
+    //for toolkit all we have to do is below the comment part is what redux will do and the 
+    // value which we will pass will be assigned to the payload (its something which is done and we arent giving this name by ourself)
+    dispatch(counterActions.increase(10));  // {type: SOME_UNIQUE_IDENTIFIER, payload: 10}
+
+    // // HARD CODED HERE <-> REMOVE (-)_(-)
+    // // Hard coded just to check it out we can make use of the input fields to remove this value
+    // dispatch({type:'increase', amount: 5})
+  };
+
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement());
+
+    // dispatch({type : 'decrement'});
+  };
+
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+
+    // dispatch({type: 'toggle'});
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+     {show && <div className={classes.value}>{counter}</div>}
+      <div>
+      <button onClick={incrementHandler}>Increment</button>
+      <button onClick={increaseHandler}>Increase by 10</button>
+      <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
 };
 
-// connect when executed will pass a function to which we then pass a argument which is our class, Also,
-// when using connect also react-redux will still setup a subscription and manage the subscription for us
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default Counter;
+
+
+// class Counter extends Component{
+
+//   incrementHandler(){
+//     this.props.increment();
+//   }
+
+//   decrementHandler(){
+//     this.props.decrement();
+//   }
+
+//   toggleCounterHandler(){
+
+//   }
+
+//   render(){
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <div>
+//           {/* we are binding to make sure the this keyword inside of the method refers to the class */}
+//         <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+//         <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+//         </div>
+//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//       </main>
+//     );
+//   }
+// }
+
+// // this is the function which receives the redux state and will 
+// const mapStateToProps = state => {
+//   /**
+//    * we pick the counter value from state and we are binding it here to the other
+//    */
+//   return {
+//     counter: state.counter
+//   };
+// }
+
+// // equivalent to useDispatch and will be executed for us by redux
+// const mapDispatchToProps = dispatch => {
+//   /**
+//    * the keys are prop names which we can then use in the component and the value is another function 
+//    * in which we call dispatch and we set-up dispatch
+//    */
+//   return {
+//     increment: () => dispatch({type: 'increment'}),
+//     decrement: () => dispatch({type: 'decrement'})
+//   }
+// };
+
+// // connect when executed will pass a function to which we then pass a argument which is our class, Also,
+// // when using connect also react-redux will still setup a subscription and manage the subscription for us
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,145 @@
+
 import {createStore} from 'redux';
 
-const countReducer = (state = { counter: 0 },action) => {
-    if(action.type === 'increment'){
-        return {
-            counter: state.counter+1
-        };
+/**
+ * we are using reduxjs/toolkit to simplify somewhat i guess
+ * When we install @reduxjs/toolkit we install redux with it so we don't need to install it seprately 
+ * i guess. 
+ * 
+ * There also is a createReducer function which allows us to create a reducer with certain enhancement
+ * but createSlice is even more powerfull than createReducer and it simplifies couple of aspects in one go
+ * 
+ * @IMP also if we have different slices then we have to provide the reducers to createStore but it would be
+ *      a problem as it should contain one arg
+ *      we could use combineReducers from redux but the toolkit provides a configureStore which
+ *      is more useful and easier i guess
+ * 
+ *      Redux expects a main reducer
+ * 
+ * @param configureStore is used for configuration of redux so we can make use of it instead of 
+ *          createStore and then it takes a obj with reducer as a key arg to which we can provide a 
+ *          object map which will then be merged internally as one big reducer 
+ */
+import {createSlice, configureStore} from '@reduxjs/toolkit';
+
+// to add a new piece of data we just need to add the data to the intial and the state we are returning
+const initialState =  {counter: 0, showCounter: true }
+
+// createSlice wants an object as a argument
+// when we have different pieces of state which are not related we could create different slices
+// but since counter and showCounter are related we are creating only one slice
+
+// Also createSlice create unique action identifier for our action 
+// 
+const counterSlice= createSlice({
+    name: 'counter', // name of the slice
+    initialState,    // using es6 key: val which are same
+    reducers: {         
+        /**
+         *  reducers is a map of all the reducers this state slice use
+         *  every method will receive a piece of state and also the action
+         *  */   
+        increment(state){
+            //we can use the below code because the toolkit will create a copy of the object and then update it
+            // in a immutable way
+            state.counter++;
+        },
+        decrement(state){
+            state.counter--;
+        },
+        increase(state, action) {
+            // payload will hold any extra data which we might be dispatching
+            state.counter = state.counter + action.payload;
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
+        }
     }
-    if(action.type === 'decrement'){
-        return {
-            counter: state.counter - 1
-        };
-    }
+});
 
-    return state;
-};
+/**
+ * These methods(toggleCounter) on the actions object here
+    which we can call will create action objects for us.
+    Therefore these methods are called action creators
+    and they will create action objects
+   -> for us where these objects already have a type property
+    with a unique identifier per action.
+    Automatically created behind the scenes.
+    So we don't have to worry about action identifiers.
 
-const store = createStore(countReducer);
+    We don't have to create those action objects on our own.
 
+    We can tap into this actions key into this actions object on our createSlice
+    and execute these action creator methods, which with their name match our reducer methods
+    to dispatch actions, which will then ultimately trigger those different reducer methods.
+    That's how this works now. And that means that we, as a developer,
+    don't have to worry about creating action objects on our own
+    and about coming up with @unique_identifiers
+    and about avoiding typos. 
+
+    Also, we are exporting thesse action and then we can go to the file where we need 
+    the actions (in our case rn the counter.js file)
+ */
+// counterSlice.actions.toggleCounter
+
+const store = configureStore({
+    //  reducer: { counter: counterSlice.reducer}
+    reducer: counterSlice.reducer
+});
+
+export const counterActions = counterSlice.actions;
 export default store;
+
+
+
+// const countReducer = (state = initialState,action) => {
+
+//     /**
+//      * If we suppose forgot adding a value like showCounter
+//      * and we are returning a state then that value which we forgot will be treated
+//      * as undefined and so for example showCounter will be undefined and that means it will be false
+//      * suppose that if we forgot it in increment it will not show the value after the button gets
+//      * pressed
+//      * 
+//      * @IMP we must set the other state which we are not using so that we are 
+//      *      overriding the default state and it doesn't causes bugs
+//      */
+
+//     if(action.type === 'increment'){
+//         // SUPER IMP - we should never mutate the existing state like state.counter++ 
+//         // and always update the state in an immutable way  
+//         return {
+//             counter: state.counter+1,
+//             showCounter: state.showCounter
+//         };
+//     }
+
+//     if(action.type === 'increase'){
+//         // property used here should have the same name as to extract the data
+//         // otherwise we won't be able to extract the data
+//         return { 
+//             counter: state.counter + action.amount,
+//             showCounter: state.showCounter
+//         };
+//     }
+
+//     if(action.type === 'decrement'){
+//         return {
+//             counter: state.counter - 1,
+//             showCounter: state.showCounter
+//         };
+//     }
+
+//     if(action.type === 'toggle'){
+//         return {
+//             showCounter: !state.showCounter,
+//             counter: state.counter
+//         }
+//     }
+
+//     return state;
+// };
+
+// const store = createStore(countReducer);
+
+// export default store;
